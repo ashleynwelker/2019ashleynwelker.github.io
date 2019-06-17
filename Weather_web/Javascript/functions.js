731002b8b342cce7621e7bfb1eac131c117ad9b6
ashleynwelker <ray13008@byui.edu>
2019-06-17 07:11:18 -0600

    More changes

--- Weather_web/Javascript/functions.js
+++ Weather_web/Javascript/functions.js
@@ -7,22 +7,22 @@ const direction = "E";
 // Calculate the Wind Chill
 function buildWC(speed, temp) {
 
-const feelTemp = document.getElementById('feelTemp');
+    const feelTemp = document.getElementById('feelTemp');
 
-// Compute the Windchill
-let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
- console.log(wc);
+    // Compute the Windchill
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    console.log(wc);
 
- // Round the answer down to integer
- wc = Math.floor(wc); 
+    // Round the answer down to integer
+    wc = Math.floor(wc); 
 
- // If chill is greater than temp, return the temp
- wc - (wc > temp)?temp:wc; 
+    // If chill is greater than temp, return the temp
+    wc - (wc > temp)?temp:wc; 
 
- // Display the windchill
- console.log(wc);
- wc = "Feels like" + wc + "&deg;F";
- feelTemp.innerHTML = wc;
+    // Display the windchill
+    console.log(wc);
+    wc = "Feels like" + wc + "&deg;F";
+    feelTemp.innerHTML = wc;
 }
 
 buildWC(speed,temp);
@@ -30,49 +30,49 @@ buildWC(speed,temp);
 // Wind Dial Function
 function windDial(direction){
 
-// Get the wind dial container
-const dial = document.getElementById("dial");
-console.log(direction);
+    // Get the wind dial container
+    const dial = document.getElementById("dial");
+    console.log(direction);
 
-// Determine the dial class
-switch (direction){
-    case "North":
-    case "N": 
-      dial.setAttribute("class", "n");
-      break;
-    case "NE":
-    case "NNE":
-    case "ENE":
-      dial.setAttribute("class", "ne");
-      break;
-    case "NW":
-    case "NNW":
-    case "WNW":
-      dial.setAttribute("class", "nw");
-      break;
-    case "South":
-    case "S":
-      dial.setAttribute("class", "s");
-      break;;
-    case "SE":
-    case "SSE":
-    case "ESE":
-      dial.setAttribute("class", "se");
-      break;
-    case "SW":
-    case "SSW":
-    case "WSW":
-      dial.setAttribute("class", "sw");
-      break;
-    case "East":
-    case "E":
-      dial.setAttribute("class", "e");
-      break;
-    case "West":
-    case "W":
-      dial.setAttribute("class", "w");
-      break;
-}
+    // Determine the dial class
+    switch (direction){
+        case "North":
+        case "N": 
+        dial.setAttribute("class", "n");
+        break;
+        case "NE":
+        case "NNE":
+        case "ENE":
+        dial.setAttribute("class", "ne");
+        break;
+        case "NW":
+        case "NNW":
+        case "WNW":
+        dial.setAttribute("class", "nw");
+        break;
+        case "South":
+        case "S":
+        dial.setAttribute("class", "s");
+        break;;
+        case "SE":
+        case "SSE":
+        case "ESE":
+        dial.setAttribute("class", "se");
+        break;
+        case "SW":
+        case "SSW":
+        case "WSW":
+        dial.setAttribute("class", "sw");
+        break;
+        case "East":
+        case "E":
+        dial.setAttribute("class", "e");
+        break;
+        case "West":
+        case "W":
+        dial.setAttribute("class", "w");
+        break;
+    }
 }
 
 windDial(direction);

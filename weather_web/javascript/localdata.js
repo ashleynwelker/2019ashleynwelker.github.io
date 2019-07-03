0f65b9f59a72210cdfc0ada483755dc1238d00a9
ashleynwelker <ray13008@byui.edu>
2019-07-03 15:16:57 -0600

    fixed alias

--- weather_web/javascript/localdata.js
+++ weather_web/javascript/localdata.js
@@ -1,3 +1,21 @@
+let pageNav = document.getElementById('page-nav');
+let statusContainer = document.getElemenyById('status');
+let contentContainer = document.getElementById('main-content');
+
+pageNav.addEventListener('click', function(evt){
+
+    // Get the city name
+let cityName = evt.target.innerHTML;
+switch (cityName) {
+ case "Franklin":
+  case "Greenville":
+   case "Springfield":
+    evt.preventDefault();
+  break;
+
+}
+
+}
 // Get the temperature data
 let locTemp = g.Temp;
 console.log(locTemp);
@@ -12,3 +30,9 @@ document.getElementById('').innerHTML = locTemp;
 // Set the wind information
 document.getElementById('').innerHTML = wind;
 buildWC(wind,locTemp);
+
+
+
+
+
+
